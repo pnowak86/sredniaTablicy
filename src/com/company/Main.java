121a64e43fff2ffabd1de6a92e0d3d2c3238958d
pnowak86 <programowanie123>
2017-04-26 12:54:35 +0200

    drobne poprawki, usuniecie zbednego kodu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,33 +4,37 @@ public class Main {
 
 
     public static int dlugosc = 100;
+    //tworzymy tablice z 100 indeksow
     public static int[] tablica = new int[dlugosc];
-    public static double wynik= 0.0;
-    public static int i = 0;
+   
+
 
     public static void main(String[] args) {
 
+
         System.out.println("Zawartość tablicy:");
         for (int i = 0; i < dlugosc - 1; i++) {
+            //zapelnialniamy wartosciami od 1 do 99
             tablica[i] = i + 1;
-
+            //drukujemy wartosci z tablicy
             System.out.println(tablica[i]);
         }
         System.out.println("");
         obliczSrednia();
-        //System.out.println(wynik);
 
 
 
     }
-   public static void obliczSrednia() {
-        double wynik =0;
+
+    //tworzymy metode obliczajaca srednia
+    public static void obliczSrednia() {
+        double wynik = 0;
         for (int i = 0; i < dlugosc; i++) {
-         wynik = wynik + tablica[i];
+            wynik = wynik + tablica[i];
 
         }
-       System.out.println("Średnia arytmetyczna tablicy z setką indeksów wynosi: " + wynik/tablica.length);
+        System.out.println("Średnia arytmetyczna tablicy z setką indeksów wynosi: " + wynik / tablica.length);
 
-   }
+    }
 
 }
